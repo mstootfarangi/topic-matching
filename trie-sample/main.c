e55050302300d290234750243b5d357f3349ed32
Farzaneh <farzaneh@firstsource.io>
2018-10-28 16:07:32 +0330

    Initial Commit

---
+++ trie-sample/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  trie-sample
+//
+//  Created by Farzaneh on 10/28/18.
+//  Copyright © 2018 Farzaneh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
